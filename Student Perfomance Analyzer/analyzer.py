46c8666a4fc0b269c50e7b030065c271b2cf71e0
abhinav-1067 <abhinavyadav1673@gmail.com>
2026-04-30 15:14:44 -0400

    student performance analysis complete

--- Student Perfomance Analyzer/analyzer.py
+++ Student Perfomance Analyzer/analyzer.py
@@ -7,6 +7,30 @@ def load_data():
 
 def pass_fail():
     data = load_data()
+    name = input("Enter Students Name: ")
+    cursor = data.cursor()
     
-    if 
     
+    cursor.execute(f'''
+    SELECT marks FROM students WHERE name="",({name})              
+                   ''')
+    result = cursor.fetchone()
+    
+    if result >= '34':
+        print(f"{name} Is Passed")
+    elif result < '34':
+        print(f"{name} Is Failed!")
+    else:
+        print("Invalid Name !!")
+        
+def mean_marks():
+    data = load_data()
+    cursor = data.cursor()
+    
+    cursor.execute('''
+    SELECT AVG(marks) FORM students               
+                   ''')
+    
+    result = cursor.fetchone()
+    
+    return result
